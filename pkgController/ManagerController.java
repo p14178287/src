0fd6461293c5442fb7efad275a86d81138678e85
shingiraimarikasi <shingiraimarikasi@192.168.0.11>
2017-02-26 21:23:34 +0000

    Displaying customers and tools info
    fully.

--- pkgController/ManagerController.java
+++ pkgController/ManagerController.java
@@ -18,9 +18,12 @@ import javafx.util.Duration;
 import main.ApplicationLoader;
 import pkgDAO.DAOfunctions;
 import pkgModel.Customer;
+import pkgModel.Tool;
 import pkgView.ButtonPanes.MGcustomerButtonPane;
+import pkgView.ButtonPanes.VerticalToolBar;
 import pkgView.Modules.CustomerPane;
 import pkgView.Modules.EditCustomerDetailsPane;
+import pkgView.Modules.ToolPane;
 import pkgView.Users.ManagerRootPane;
 //import pkgView.Users.ResetPane;
 
@@ -29,40 +32,52 @@ public class ManagerController {
 	 * Fields declared as global mutable variables within the class so that they
 	 * are accessible throughout the whole class
 	 */
+	private VerticalToolBar verticalToolBar;
 	private ManagerRootPane managerRootPane;
 	private CustomerPane customerPane;
-	private ObservableList<Customer> model;
+	private ToolPane toolPane;
+	private ObservableList<Customer> modelCustomer;
+	private ObservableList<Tool> modelTool;
 	private Connection myConnection;
 	private MGcustomerButtonPane mgCustomerButtonPane;
-	private ApplicationLoader loader;
+	// private ApplicationLoader loader;
 	private EditCustomerDetailsPane editCustomerDetailsPane;
-	private EditCustomerDetailsController editCustomerDetailsController;
+	// private EditCustomerDetailsController editCustomerDetailsController;
 	private Integer uniqueCustomerID, updatedCustomerPhoneNumber;
 	private String updatedCustomerFirstName, updatedCustomerLastName, updatedCustomerAddress, updatedCustomerEmail;
 	private Statement myStatement = null;
 	private ResultSet myResult = null;
 	private DAOfunctions connectdao;
 
-	public ManagerController(ManagerRootPane managerRootPane, ObservableList<Customer> model) {
+	public ManagerController(ManagerRootPane managerRootPane, ObservableList<Customer> modelCustomer,
+			ObservableList<Tool> modelTool) {
 		this.managerRootPane = managerRootPane;
-		this.model = model;
+		this.modelCustomer = modelCustomer;
+		this.modelTool = modelTool;
 		customerPane = managerRootPane.getCustomerPane();
+		toolPane = managerRootPane.getToolPane();
+		verticalToolBar = managerRootPane.getVerticalToolBar();
 		editCustomerDetailsPane = customerPane.getEditCustomerDetailsPane();
 		mgCustomerButtonPane = customerPane.getmGcustomerButtonPane();
 		this.attachEventHandlers();
 	}
 
 	private void attachEventHandlers() {
-		customerPane.attachSearchlCustomersBtnHandler(new ViewAllCustomersBtnEventHandler());
 
+		/*****************************
+		 * CUSTOMERPANE EVENT HANDLERS
+		 *****************************/
+
+		customerPane.attachSearchlCustomersBtnHandler(new ViewAllCustomersBtnEventHandler());
+		// verticalToolBar.attachToolListsBtnHandler(new
+		// toolListBtnEventHandler());
 		/*----------------------------------------------------------------
 		 * 1. EVENTHANDLER FUNCTION 1. A listener to show customer details
 		 *---------------------------------------------------------------*/
-		
+
 		/*
-		 * Listen to change of customer row selected 
-		 * and display it in the right side of the 
-		 * SpiltPane
+		 * Listen to change of customer row selected and display it in the right
+		 * side of the SpiltPane
 		 */
 		customerPane.getTableView().getSelectionModel().selectedItemProperty()
 				.addListener((model, oldValue, newValue) -> showCustomerDetails(newValue));
@@ -72,7 +87,7 @@ public class ManagerController {
 		 *---------------------------------------------------------------*/
 		customerPane.getspCustomerHyperlinkEditor().setOnAction(e -> {
 			PopOver popover = new PopOver();
-			editCustomerDetailsPane = new EditCustomerDetailsPane();		
+			editCustomerDetailsPane = new EditCustomerDetailsPane();
 			/*
 			 * When first shown, display customer details currently highlighted.
 			 * and subsequently listen for any changes of the highlighted
@@ -83,7 +98,7 @@ public class ManagerController {
 					.addListener((model, oldValue, newValue) -> showCustomerDetailsInPopoverEditor(newValue));
 
 			popover.setContentNode(editCustomerDetailsPane);
-			// 
+			//
 			editCustomerDetailsPane.getSaveCustomerEditedDetails().setOnAction(param -> {
 				uniqueCustomerID = Integer.parseInt(editCustomerDetailsPane.getCustomerID().getText());
 				updatedCustomerFirstName = editCustomerDetailsPane.getCustomerFirstName().getText();
@@ -95,43 +110,90 @@ public class ManagerController {
 				DAOfunctions.passDBconnection();
 				DAOfunctions.updateNewValuesEntered(uniqueCustomerID, updatedCustomerFirstName, updatedCustomerLastName,
 						updatedCustomerAddress, updatedCustomerEmail, updatedCustomerPhoneNumber);
-				System.out.println("being executed " + uniqueCustomerID + " " + updatedCustomerFirstName + " " + updatedCustomerPhoneNumber);
+				System.out.println("being executed " + uniqueCustomerID + " " + updatedCustomerFirstName + " "
+						+ updatedCustomerPhoneNumber);
 			});
-			
+
 			popover.fadeInDurationProperty().set(Duration.millis(800));
 			popover.show(customerPane.getspCustomerHyperlinkEditor());
 			popover.setTitle("Edit Customer Details");
 			popover.setDetached(true);
 		});
-		
+
 		/*-----------------------------------------------------------------------
 		 *  3. LEFT SIDE EVENTHANDLER FUNCTION: Insert new customer into Database
 		 *----------------------------------------------------------------------*/
-		
+
 		mgCustomerButtonPane.getCreatNewCustomerBtn().setOnAction(event -> {
-	
-		// 1. Fetch values from the TextFields.
-		Integer id = Integer.parseInt(customerPane.getCustomerIDLeftTX().toString());
-		String firstName = customerPane.getCustomerFirstNameLeftTX();
-		String surname = customerPane.getCcustomerLastNameLeftTX();
-		String address = customerPane.getCustomerAddressLeftTX();
-		String email = customerPane.getCustomerEmailLeftTX();
-		Integer phoneNumber = Integer.parseInt(customerPane.getCustomerPhoneNoLeftTX());
-		
-		// 2. Insert values into model customer ObservablList ready to display in TableView.
-		Customer newCustomer = new Customer(id.intValue(), firstName, surname, address, email, phoneNumber.intValue());
-		model.add(newCustomer);
-			customerPane.getTableView().setItems(model);
-			
-		// 3. Insert the values into the database.
+
+			// 1. Fetch values from the TextFields.
+			Integer id = Integer.parseInt(customerPane.getCustomerIDLeftTX().toString());
+			String firstName = customerPane.getCustomerFirstNameLeftTX();
+			String surname = customerPane.getCcustomerLastNameLeftTX();
+			String address = customerPane.getCustomerAddressLeftTX();
+			String email = customerPane.getCustomerEmailLeftTX();
+			Integer phoneNumber = Integer.parseInt(customerPane.getCustomerPhoneNoLeftTX());
+
+			// 2. Insert values into model customer ObservablList ready to
+			// display in TableView.
+			Customer newCustomer = new Customer(id.intValue(), firstName, surname, address, email,
+					phoneNumber.intValue());
+			modelCustomer.add(newCustomer);
+			customerPane.getTableView().setItems(modelCustomer);
+
+			// 3. Insert the values into the database.
 			connectdao = new DAOfunctions();
 			DAOfunctions.passDBconnection();
 			connectdao.insertNewCustomer(id.intValue(), firstName, surname, address, email, phoneNumber.intValue());
-			
-		// 4. Clear the TextField
-			//customerPane.getCustomerFirstNameLeftTX();
+
+			// 4. Clear the TextField
+			// customerPane.getCustomerFirstNameLeftTX();
 		});
-		
+
+		/*
+		 * Seems to be working when attached to view all customers button in
+		 * toolpane, which is still undesirable. TODO: investigate how to attach
+		 * to the button in the vertcalToolBar and get the same results.
+		 */
+		toolPane.getViewAllTools().setOnAction(value -> {
+			// @Override
+			// public void handle(ActionEvent event) {
+			myConnection = DAOfunctions.passDBconnection();
+			modelTool = FXCollections.observableArrayList();
+			try {
+				myStatement = myConnection.createStatement();
+				myResult = myStatement.executeQuery("select * from dth_Tool;");
+				while (myResult.next()) {
+					addToolData(myResult);
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("sql error");
+				e.printStackTrace();
+			} finally {
+				FilteredList<Tool> filteredData = new FilteredList<>(modelTool, p -> true);
+				toolPane.getSearchTF().textProperty().addListener((observable, oldValue, newValue) -> {
+					filteredData.setPredicate(tool -> {
+						// If filter text is empty, display all persons.
+						if (newValue == null || newValue.isEmpty()) {
+							return true;
+						}
+						String lowerCaseFilter = newValue.toLowerCase();
+						if (tool.getDbToolName().toLowerCase().contains(lowerCaseFilter)) {
+							return true; // Filter matches first name.
+						} else if (tool.getDbTypeName().toLowerCase().contains(lowerCaseFilter)) {
+							return true; // Filter matches last name.
+						}
+						return false; // Does not match.
+					});
+				});
+				SortedList<Tool> sortedData = new SortedList<>(filteredData);
+				toolPane.getTableView().setItems(sortedData);
+				managerRootPane.fadeAnimation();
+			}
+
+		});
+
 	}
 
 	private class ViewAllCustomersBtnEventHandler implements EventHandler<ActionEvent> {
@@ -145,7 +207,7 @@ public class ManagerController {
 			myConnection = DAOfunctions.passDBconnection();
 			// Statement myStatement = null;
 			// ResultSet myResult = null;
-			model = FXCollections.observableArrayList();
+			modelCustomer = FXCollections.observableArrayList();
 			try {
 				myStatement = myConnection.createStatement();
 				myResult = myStatement.executeQuery("select * from Customer;");
@@ -166,7 +228,7 @@ public class ManagerController {
 				 * Wrap the ObservablearrayList in a FilteredList and initially
 				 * display all customer data.
 				 */
-				FilteredList<Customer> filteredData = new FilteredList<>(model, p -> true);
+				FilteredList<Customer> filteredData = new FilteredList<>(modelCustomer, p -> true);
 
 				// Set the filter Predicate whenever the filter changes.
 				customerPane.getSearchTF().textProperty().addListener((observable, oldValue, newValue) -> {
@@ -203,6 +265,57 @@ public class ManagerController {
 		}
 
 	}
+
+	/**
+	 * EventHandler to instantly display the tool list, when the user clicks the
+	 * tool button to switch to the view. once the button is clicked all tools
+	 * in the database are displayed. This seems desirable rather than opening
+	 * the pane and then clicking on the button to show the List.
+	 */
+
+	// private class toolListBtnEventHandler implements
+	// EventHandler<ActionEvent> {
+	// @Override
+	// public void handle(ActionEvent event) {
+	// myConnection = DAOfunctions.passDBconnection();
+	// modelTool = FXCollections.observableArrayList();
+	// try {
+	// myStatement = myConnection.createStatement();
+	// myResult = myStatement.executeQuery("select * from dth_Tool;");
+	// while (myResult.next()) {
+	// addToolData(myResult);
+	// }
+	// } catch (SQLException e) {
+	// // TODO Auto-generated catch block
+	// System.out.println("sql error");
+	// e.printStackTrace();
+	// } finally {
+	// FilteredList<Tool> filteredData = new FilteredList<>(modelTool, p ->
+	// true);
+	// toolPane.getSearchTF().textProperty().addListener((observable, oldValue,
+	// newValue) -> {
+	// filteredData.setPredicate(tool -> {
+	// // If filter text is empty, display all persons.
+	// if (newValue == null || newValue.isEmpty()) {
+	// return true;
+	// }
+	// String lowerCaseFilter = newValue.toLowerCase();
+	// if (tool.getDbToolName().toLowerCase().contains(lowerCaseFilter)) {
+	// return true; // Filter matches first name.
+	// } else if (tool.getDbTypeName().toLowerCase().contains(lowerCaseFilter))
+	// {
+	// return true; // Filter matches last name.
+	// }
+	// return false; // Does not match.
+	// });
+	// });
+	// SortedList<Tool> sortedData = new SortedList<>(filteredData);
+	// toolPane.getTableView().setItems(sortedData);
+	// managerRootPane.fadeAnimation();
+	// }
+	// }
+	//
+	// }
 
 	/**** UTILITY FUNCTIONS ****/
 
@@ -251,14 +364,45 @@ public class ManagerController {
 			String email = myResult.getString("dbCustomerEmail");
 			Integer phoneNumber = myResult.getInt("dbCustomerPhoneNumber");
 
-			model.add(new Customer(customerNo, firstName, lastName, address, email, phoneNumber));
+			modelCustomer.add(new Customer(customerNo, firstName, lastName, address, email, phoneNumber));
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		return model;
+		return modelCustomer;
+	}
+
+	private final ObservableList<Tool> addToolData(ResultSet myResult) {
+		try {
+			Integer dbToolID = myResult.getInt("dbToolID");
+			String dbToolName = myResult.getString("dbToolName");
+			String dbTypeName = myResult.getString("dbTypeName");
+			String dbToolDescription = myResult.getString("dbToolDescription");
+			Integer dbStockQuantityAvailable = myResult.getInt("dbStockQuantityAvailable");
+			Integer dbQuantityInService = myResult.getInt("dbQuantityInService");
+			Integer dbQuantityOnHire = myResult.getInt("dbQuantityOnHire");
+			Integer dbMaxHirePeriod = myResult.getInt("dbMaxHirePeriod");
+			String dbDateOfHire = myResult.getString("dbDateOfHire");
+			String dbDueDateOfReturn = myResult.getString("dbDueDateOfReturn");
+			String dbDateOfActualReturn = myResult.getString("dbDateOfActualReturn");
+			String dbConditionOnHire = myResult.getString("dbConditionOnHire");
+			String dbConditionOnReturn = myResult.getString("dbConditionOnReturn");
+			Double dbToolHirepricePerDay = myResult.getDouble("dbToolHirepricePerDay");
+			String dbServiceDueDate = myResult.getString("dbServiceDueDate");
+
+			modelTool.add(new Tool(dbToolID, dbToolName, dbTypeName, dbToolDescription, dbStockQuantityAvailable,
+					dbQuantityInService, dbQuantityOnHire, dbMaxHirePeriod, dbDateOfHire, dbDueDateOfReturn,
+					dbDateOfActualReturn, dbConditionOnHire, dbConditionOnReturn, dbToolHirepricePerDay,
+					dbServiceDueDate));
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return modelTool;
 	}
 
 }
